de0611c4bb0c760f6f83bcaf4fee7f94d63f29b9
张文宁 <zhangwenning@lingxi.co>
2017-09-02 23:12:49 +0800

    :tada: update file

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ app.get('/movieA', (req, res) => {
 	const fileSize = stat.size;
 	const range = req.headers.range;
 
-	console.log('########', range);
-
 	const parts = range.replace(/bytes=/, "").split("-")
 	const start = parseInt(parts[0], 10);
 	const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
